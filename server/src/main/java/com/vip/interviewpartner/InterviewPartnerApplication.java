90fe40ab48592cb11348428cd04f30576462b145
junest66 <chlwndks333@gmail.com>
2024-04-30 04:04:21 +0900

    chore: 백엔드 기본 프로젝트 설정
    #1

---
+++ server/src/main/java/com/vip/interviewpartner/InterviewPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.vip.interviewpartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPartnerApplication.class, args);
+	}
+
+}
