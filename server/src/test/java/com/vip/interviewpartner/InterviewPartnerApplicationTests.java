29a62ce9ff1b4101a205f7b5cdc592922748e283
Jun Choi <121853214+junest66@users.noreply.github.com>
2024-05-01 23:19:07 +0900

    chore: 백엔드 기본 프로젝트 설정 (#2)
    #1

---
+++ server/src/test/java/com/vip/interviewpartner/InterviewPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vip.interviewpartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
