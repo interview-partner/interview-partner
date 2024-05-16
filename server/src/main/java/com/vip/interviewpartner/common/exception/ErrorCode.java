ca534dbb8ea6a1089f5c3c38eaede3173d45ae97
junest66 <chlwndks333@gmail.com>
2024-05-16 15:24:48 +0900

    feat: 지원하지 않는 소셜 에러코드 추가
    #28

--- server/src/main/java/com/vip/interviewpartner/common/exception/ErrorCode.java
+++ server/src/main/java/com/vip/interviewpartner/common/exception/ErrorCode.java
@@ -20,7 +20,8 @@ public enum ErrorCode {
     REFRESH_TOKEN_EXPIRED(401, "리프레쉬 토큰이 만료되었습니다."),
     INVALID_TOKEN(401, "유효하지 않는 토큰입니다."),
     REFRESH_TOKEN_NOT_EXIST(401, "리프레쉬 토큰이 존재하지 않습니다."),
-    FILE_EMPTY(400, "no file");
+    FILE_EMPTY(400, "no file"),
+    UNSUPPORTED_SOCIAL_MEDIA(400, "지원하지 않는 소셜 미디어입니다.");
 
     private final int status;
     private final String message;
