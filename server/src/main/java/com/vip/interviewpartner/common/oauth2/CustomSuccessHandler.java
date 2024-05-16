28ea2844ba8df4dfdb89e501ea82e4b2092528b9
junest66 <chlwndks333@gmail.com>
2024-05-16 15:27:37 +0900

    docs: CustomSuccessHandler 주석 추가
    #28

--- server/src/main/java/com/vip/interviewpartner/common/oauth2/CustomSuccessHandler.java
+++ server/src/main/java/com/vip/interviewpartner/common/oauth2/CustomSuccessHandler.java
@@ -15,12 +15,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+/**
+ * CustomSuccessHandler는 OAuth2 인증 성공 시 실행되는 핸들러로,
+ * 사용자 정보를 바탕으로 리프레시 토큰을 생성하고 이를 쿠키에 추가하여 클라이언트에 반환합니다.
+ */
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final TokenService tokenService;
 
+    /**
+     * 인증 성공 시 실행되는 메서드로, 사용자 정보를 기반으로 리프레시 토큰을 생성하고,
+     * 이를 쿠키에 추가하여 클라이언트에 반환합니다. 또한, 클라이언트를 특정 URL로 리다이렉트합니다.
+     *
+     * @param request  HttpServletRequest 객체
+     * @param response HttpServletResponse 객체
+     * @param authentication 인증 객체
+     * @throws IOException 예외 처리
+     * @throws ServletException 예외 처리
+     */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         Member member = ((CustomUserDetails) authentication.getPrincipal()).getMember();
