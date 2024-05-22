fe36dfa28f0aa4de0d89288e73f38294e800e43a
Ji-seung Pyun <101254480+vuswltmd@users.noreply.github.com>
2024-05-22 17:14:32 +0900

    feat: 소셜 로그인 API 적용 (#49)
    * 페이지 개발에 필요한 icon 파일 추가
    
    * style: 모의면접 채팅창에 임시로 사용할 사용자 이미지 추가
    
    #37
    
    * style: .navbar의 position fixed를 삭제
    
    * feat: Mockup community 페이지 개발
    
     - 방 조회, 생성, 입장 기능 추가
    
     #37
    
    * feat: Mockup community의 방 생성 모달 개발
    
     - 방 생성에 필요한 모달 페이지 기능 추가
    
     #37
    
    * feat: Mockup Interview 페이지 개발
    
     - 화상면접  페이지 개발
    
     #37
    
    * feat: Mockup Interview 페이지의 채팅창 기능 구현
    
     #37
    
    * feat: Mockup Community 페이지의  방에 대한 임시 데이터 추가
    
     #37
    
    * chore: .gitignore 파일 업데이트
    
    * chore: package.json 업데이트
    
     - axios 추가
     - dotenv 추가
     - firebase 추가
    
    * feat: 토큰 재발급 라우트 추가
    
     #44
    
    * refactor: fetch에서 axios로 요청 방식 변경
    
    * chore: firebase 설정 작성
    
    * chore: Axios 인터셉터 설정 파일 추가
    
     - 요청 시 액세스 토큰을 헤더에 추가하는 인터셉터 설정
     - 응답 오류 시 토큰 재발행을 시도하는 인터셉터 설정
    
     #44
    
    * feat: 소셜 로그인 API 추가
    
     - 카카오 로그인 API 추가
     - 네이버 로그인 API 추가
     - 구글 로그인 API 추가
    
     #44
    
    * feat: Google 소셜 로그인 API 적용 및 오류 처리 로직 구현
    
     - Firebase ID 토큰을 백엔드로 전송하여 로그인 처리
     - 백엔드 응답 오류 처리 로직 추가
    
     #44
    
    * feat: 토큰 재발행 컴포넌트 추가
    
     - Axios를 사용하여 토큰 재발행 요청
    
     #44

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,22 @@
-export const config = {
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const config = {
     apiUrl: 'http://localhost:8080',
 };
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { config }
+export { auth }
