62c9b97c563b6eafcac04b2737c6afdc4a9ec45c
junest66 <chlwndks333@gmail.com>
2024-05-25 00:48:43 +0900

    style: contextLoads() 함수 주석 처리

--- server/src/test/java/com/vip/interviewpartner/InterviewPartnerApplicationTests.java
+++ server/src/test/java/com/vip/interviewpartner/InterviewPartnerApplicationTests.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class InterviewPartnerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
