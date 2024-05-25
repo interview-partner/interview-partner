6591ef52dfc38e9b00f8586653b05b699edc5683
Daeuning <happyjde3@gmail.com>
2024-05-25 21:12:17 +0900

    feat: shwo-progress 폴더 생성 및 progress 확인용 컴포넌트 SelectInterviewInfo 생성
    #62

---
+++ client/src/features/show-progress/SelectInterviewInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import { COLORS } from "../../styles/colors"
+import ArrowButton from "../../components/button/arrowButton.jsx"
+
+function SelectInterviewInfo() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+  
+export default SelectInterviewInfo;
