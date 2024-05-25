5b6841127957b9c4c5c63f04a1a5c2fb8c072d00
junest66 <chlwndks333@gmail.com>
2024-05-25 23:12:13 +0900

    feat: 해당 멤버의 활성화 이력서를 조회하는 쿼리 함수 추가
    #64

--- server/src/main/java/com/vip/interviewpartner/repository/ResumeRepository.java
+++ server/src/main/java/com/vip/interviewpartner/repository/ResumeRepository.java
@@ -1,10 +1,22 @@
 package com.vip.interviewpartner.repository;
 
 import com.vip.interviewpartner.domain.Resume;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Resume 엔티티에 대한 데이터 접근 리포지토리입니다.
  */
 public interface ResumeRepository extends JpaRepository<Resume, Long> {
+    /**
+     * 특정 회원 ID와 활성 상태에 해당하는 이력서를 조회합니다.
+     *
+     * @param memberId 조회할 회원의 ID
+     * @param isActive 이력서의 활성 상태
+     * @return 특정 회원 ID와 활성 상태에 해당하는 이력서 리스트
+     */
+    @Query("SELECT r FROM Resume r WHERE r.member.id = :memberId AND r.isActive = :isActive")
+    List<Resume> findByMemberIdAndIsActive(Long memberId, boolean isActive);
 }
