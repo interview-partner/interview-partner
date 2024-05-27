e86dacd6ed9c09b5d7ee60a4bd389810e9419a5d
zs0057 <soss4823@gmail.com>
2024-05-27 14:41:08 +0900

    refactor: Long type에 NotBlank -> NotNull로 수정
    #39

--- server/src/main/java/com/vip/interviewpartner/dto/AiInterviewRequest.java
+++ server/src/main/java/com/vip/interviewpartner/dto/AiInterviewRequest.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -35,7 +36,7 @@ public class AiInterviewRequest {
     @Schema(description = "채용공고", example = "네이버 백엔드 모집합니다.")
     private String jobAdvertisement;
 
-    @NotBlank(message = "이력서 ID는 필수입니다.")
+    @NotNull(message = "이력서 ID는 필수입니다.")
     @Schema(description = "이력서 ID", example = "1")
     private Long resumeId;
 
