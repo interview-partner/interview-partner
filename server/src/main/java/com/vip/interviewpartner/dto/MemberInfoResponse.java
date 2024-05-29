27e90aa2dcf40a52c194fbf028acd613edbbc460
Jun Choi <121853214+junest66@users.noreply.github.com>
2024-05-30 08:58:35 +0900

    feat: 자신의 회원정보 조회 API 추가 (#82)
    * feat: 회원 정보 조회 응답 DTO 추가
    
    #81
    
    * feat: 회원 정보 조회 기능 추가
    
    #81
    
    * feat: 회원 정보 조회 API 작성
    
    #81
    
    * refactor: 재사용하는 함수 사용으로 변경

---
+++ server/src/main/java/com/vip/interviewpartner/dto/MemberInfoResponse.java
@@ -0,0 +1,50 @@
+package com.vip.interviewpartner.dto;
+
+import com.vip.interviewpartner.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.format.DateTimeFormatter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 회원정보 조회 응답 DTO 클래스입니다.
+ * 이 클래스는 회원정보 조회 API의 응답 데이터를 담고 있습니다.
+ */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Schema(description = "회원정보 조회 응답 DTO")
+public class MemberInfoResponse {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+
+    @Schema(description = "회원의 고유 ID", example = "1")
+    private Long id;
+
+    @Schema(description = "회원 이메일(로그인 ID)", example = "user@example.com")
+    private String email;
+
+    @Schema(description = "회원 닉네임", example = "nickname")
+    private String nickname;
+
+    @Schema(description = "회원 가입일", example = "2024.05.24 14:30")
+    private String createdDate;
+
+    /**
+     * Member Entity -> MemberInfoResponse DTO 변환하는 메서드입니다.
+     *
+     * @param member 변환할 Member 엔티티
+     * @return 변환된 MemberInfoResponse 객체
+     */
+    public static MemberInfoResponse of(Member member) {
+        return MemberInfoResponse.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .createdDate(member.getCreateDate().format(formatter))
+                .build();
+    }
+
+}
