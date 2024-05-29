03f587f4a7dc776cd4393ffe8d1dad927a87eaee
junest66 <chlwndks333@gmail.com>
2024-05-30 00:07:54 +0900

    feat: 중복된 피드백을 검사하는 레포지토리 함수 추가
    #78

--- server/src/main/java/com/vip/interviewpartner/repository/FeedbackRepository.java
+++ server/src/main/java/com/vip/interviewpartner/repository/FeedbackRepository.java
@@ -1,10 +1,19 @@
 package com.vip.interviewpartner.repository;
 
 import com.vip.interviewpartner.domain.Feedback;
+import com.vip.interviewpartner.domain.RoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Feedback 엔티티에 대한 데이터 접근 리포지토리입니다.
  */
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    /**
+     * 주어진 보낸 사람과 받는 사람 간에 이미 존재하는 피드백이 있는지 확인합니다.
+     *
+     * @param sender 피드백의 보낸 사람인 RoomParticipant 객체
+     * @param receiver 피드백의 받는 사람인 RoomParticipant 객체
+     * @return 보낸 사람과 받는 사람 조합으로 이미 존재하는 피드백이 있으면 true, 없으면 false를 반환합니다.
+     */
+    boolean existsBySenderAndReceiver(RoomParticipant sender, RoomParticipant receiver);
 }
