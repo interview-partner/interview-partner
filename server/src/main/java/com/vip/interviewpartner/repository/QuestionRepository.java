e1b00ad9989dbafa4f640e8750b10e4785d0f506
zs0057 <soss4823@gmail.com>
2024-05-30 18:07:12 +0900

    feat: 인터뷰 ID로 질문 엔티티 접근하는 기능 추가
    #84

--- server/src/main/java/com/vip/interviewpartner/repository/QuestionRepository.java
+++ server/src/main/java/com/vip/interviewpartner/repository/QuestionRepository.java
@@ -3,8 +3,11 @@ package com.vip.interviewpartner.repository;
 import com.vip.interviewpartner.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Question 엔티티에 대한 데이터 접근 리포지토리입니다.
  */
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByInterviewId(Long interviewId);
 }
