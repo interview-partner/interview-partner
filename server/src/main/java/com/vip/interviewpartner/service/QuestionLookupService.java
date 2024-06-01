7d14089f4277802354828455303173f3ab394665
zs0057 <soss4823@gmail.com>
2024-06-01 12:13:15 +0900

    refactor: 변수명 변경 및 사용하지 않은 import 제거
    #87

--- server/src/main/java/com/vip/interviewpartner/service/QuestionLookupService.java
+++ server/src/main/java/com/vip/interviewpartner/service/QuestionLookupService.java
@@ -2,11 +2,9 @@ package com.vip.interviewpartner.service;
 
 import com.vip.interviewpartner.common.exception.CustomException;
 import com.vip.interviewpartner.common.exception.ErrorCode;
-import com.vip.interviewpartner.domain.Member;
 import com.vip.interviewpartner.domain.Question;
 import com.vip.interviewpartner.dto.CustomUserDetails;
 import com.vip.interviewpartner.dto.QuestionLookupResponse;
-import com.vip.interviewpartner.repository.InterviewRepository;
 import com.vip.interviewpartner.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +17,7 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class QuestionLookupService {
     private final QuestionRepository questionRepository;
-    private final InterviewService InterviewService;
+    private final InterviewService interviewService;
 
     /**
      * 주어진 인터뷰 ID에 해당하는 질문 목록들을 조회합니다.
@@ -28,7 +26,7 @@ public class QuestionLookupService {
      * @return 주어진 인터뷰 ID에 해당하는 질문 목록을 담은 QuestionLookupResponse 리스트
      */
     public List<QuestionLookupResponse> getQuestionsByInterviewId(CustomUserDetails customUserDetails, Long interviewId) {
-        InterviewService.validateInterviewOwnership(customUserDetails.getMemberId(), interviewId);
+        interviewService.validateInterviewOwnership(customUserDetails.getMemberId(), interviewId);
         List<Question> questions = questionRepository.findByInterviewId(interviewId);
         if (questions.isEmpty()) {
             throw new CustomException(ErrorCode.INVALID_REQUEST);
