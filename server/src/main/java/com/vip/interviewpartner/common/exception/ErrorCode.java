1b5610b0d1c60086f8fba4b01ef21a0711addc41
zs0057 <soss4823@gmail.com>
2024-06-01 09:58:39 +0900

    feat: 에러코드 추가
    #87

--- server/src/main/java/com/vip/interviewpartner/common/exception/ErrorCode.java
+++ server/src/main/java/com/vip/interviewpartner/common/exception/ErrorCode.java
@@ -21,6 +21,7 @@ public enum ErrorCode {
     UNSUPPORTED_SOCIAL_MEDIA(400, "지원하지 않는 소셜 미디어입니다."),
     INVALID_FIREBASE_ID_TOKEN(400, "유효하지 않는 Firebase 토큰입니다."),
     QUESTION_NOT_FOUND(400, "유효하지 않는 질문입니다."),
+    INTERVIEW_NOT_FOUND(400, "유효하지 않는 인터뷰 입니다."),
     AUTHENTICATION_REQUIRED(401, "인증이 필요합니다."),
     LOGIN_FAILURE(401, "로그인에 실패했습니다."),
     ACCESS_TOKEN_EXPIRED(401, "엑세스 토큰이 만료되었습니다."),
