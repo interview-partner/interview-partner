1c60a4fa6664dd60766f2a5c8ce7fc3c0321b3ed
Daeuning <happyjde3@gmail.com>
2024-06-04 14:24:03 +0900

    feat: 인터뷰 데이터 조회 api 적용
    #107

--- client/src/features/banner/interviewInfo.jsx
+++ client/src/features/banner/interviewInfo.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { COLORS } from "../../styles/colors";
 import ArrowButton from "../../components/button/arrowButton.jsx";
+import { getInterviewInfo } from '../../services/getInterviewInfoService';
+import { InterviewContext } from '../../context/interviewContext';
 
 const BannerContainer = styled.div`
   display: flex;
@@ -39,6 +41,47 @@ const Info = styled.div`
   margin-bottom: 8px;
   color: ${COLORS.gray};
   font-size: 14px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Tooltip = styled.div`
+  visibility: hidden;
+  width: auto;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%; /* Position the tooltip above the text */
+  left: 50%;
+  margin-left: -60px;
+  opacity: 0;
+  transition: opacity 0.3s;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: black transparent transparent transparent;
+  }
+`;
+
+const InfoWithTooltip = styled.div`
+  position: relative;
+  display: inline-block;
+
+  &:hover ${Tooltip} {
+    visibility: visible;
+    opacity: 1;
+  }
 `;
 
 const ApplyContainer = styled.div`
@@ -55,18 +98,34 @@ const ButtonContainer = styled.div`
 `;
 
 function InterviewInfo() {
+  const { interviewId } = useContext(InterviewContext);
   const [interviewData, setInterviewData] = useState(null);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('interviewData'));
-    setInterviewData(data);
-  }, []);
+    const fetchInterviewInfo = async () => {
+      try {
+        const data = await getInterviewInfo(interviewId);
+        setInterviewData(data.data); // Assuming the response structure is { status, data, message }
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    if (interviewId) {
+      fetchInterviewInfo();
+    }
+  }, [interviewId]);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   if (!interviewData) {
     return <div>Loading...</div>;
   }
 
-  const formattedDate = new Date(interviewData.createdAt).toLocaleDateString('ko-KR', {
+  const formattedDate = new Date(interviewData.createDate).toLocaleDateString('ko-KR', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit'
@@ -81,7 +140,10 @@ function InterviewInfo() {
         <Title>{interviewData.title}</Title>
         <Info>면접 일자&nbsp;|&nbsp;{formattedDate}</Info>
         <Info>면접 방식&nbsp;|&nbsp;{interviewType}</Info>
-        <Info>이력서&nbsp;|&nbsp;{interviewData.original_file_name}</Info>
+        <InfoWithTooltip>
+          <Info>이력서&nbsp;|&nbsp;{interviewData.resumeFileName}</Info>
+          <Tooltip>{interviewData.resumeFileName}</Tooltip>
+        </InfoWithTooltip>
         <ButtonContainer>
           <ArrowButton to="back">수정 하기</ArrowButton>
         </ButtonContainer>
