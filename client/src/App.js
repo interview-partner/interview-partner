9ccd2878d15463cb779e2e514e3030bb56c160c9
Daeuning <happyjde3@gmail.com>
2024-06-04 10:49:50 +0900

    feat: promptroom라우팅 설정
    #105

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Signup from "../src/pages/signup/signup.jsx";
 import Voiceroom from "../src/pages/voiceroom/voiceroom.jsx";
 import { AuthProvider } from './context/AuthContext.js';
 import { AccountProvider } from './context/accountContext';
+import { InterviewProvider } from './context/interviewContext'; // InterviewProvider import
 
 /**
  * 메인 애플리케이션 컴포넌트
@@ -23,29 +24,31 @@ function App() {
     <>
       <AuthProvider>
         <AccountProvider>
-          {/* 네비게이션 바 컴포넌트 */}
-          <Navbar />
-          {/* 라우트 설정 */}
-          <Routes>
-            {/* 메인 페이지 */}
-            <Route path="/" element={<Main />} />
-            {/* AI 면접 페이지 */}
-            <Route path="/aiinterview" element={<AIinterview />} />
-            {/* 프롬프트면접방 페이지 */}
-            <Route path="/promptroom" element={<Promptroom />} />
-            {/* 음성면접방 페이지 */}
-            <Route path="/voiceroom" element={<Voiceroom />} />
-            {/* 로그인 페이지 */}
-            <Route path="/login" element={<Login />} />
-            {/* 목업 커뮤니티 페이지 */}
-            <Route path="/mockupcommunity" element={<Mockupcommunity />} />
-            {/* 목업방 페이지 */}
-            <Route path="/mockuproom" element={<Mockuproom />} />
-            {/* 마이페이지 */}
-            <Route path="/mypage" element={<Mypage />} />
-            {/* 회원가입 페이지 */}
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
+          <InterviewProvider> {/* InterviewProvider 추가 */}
+            {/* 네비게이션 바 컴포넌트 */}
+            <Navbar />
+            {/* 라우트 설정 */}
+            <Routes>
+              {/* 메인 페이지 */}
+              <Route path="/" element={<Main />} />
+              {/* AI 면접 페이지 */}
+              <Route path="/aiinterview" element={<AIinterview />} />
+              {/* 프롬프트면접방 페이지 */}
+              <Route path="/promptroom/:id" element={<Promptroom />} />
+              {/* 음성면접방 페이지 */}
+              <Route path="/voiceroom" element={<Voiceroom />} />
+              {/* 로그인 페이지 */}
+              <Route path="/login" element={<Login />} />
+              {/* 목업 커뮤니티 페이지 */}
+              <Route path="/mockupcommunity" element={<Mockupcommunity />} />
+              {/* 목업방 페이지 */}
+              <Route path="/mockuproom" element={<Mockuproom />} />
+              {/* 마이페이지 */}
+              <Route path="/mypage" element={<Mypage />} />
+              {/* 회원가입 페이지 */}
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </InterviewProvider> {/* InterviewProvider 닫기 */}
         </AccountProvider>
       </AuthProvider>
     </>
