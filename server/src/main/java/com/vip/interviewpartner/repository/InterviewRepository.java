91c82a2f9761f16a3d969fe1a70b81a1b4d912d5
zs0057 <soss4823@gmail.com>
2024-06-06 19:03:24 +0900

    feat: 멤버 ID에 따른 인터뷰들을 반환하는 메서드 추가
    #112

--- server/src/main/java/com/vip/interviewpartner/repository/InterviewRepository.java
+++ server/src/main/java/com/vip/interviewpartner/repository/InterviewRepository.java
@@ -2,8 +2,11 @@ package com.vip.interviewpartner.repository;
 
 import com.vip.interviewpartner.domain.Interview;
 import com.vip.interviewpartner.domain.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -19,5 +22,15 @@ public interface InterviewRepository extends JpaRepository<Interview, Long> {
      */
     @Query("SELECT i.member FROM Interview i WHERE i.id = :id")
     Optional<Member> findMemberById(Long id);
+
+    /**
+     * 멤버 ID가 주어지면 해당하는 인터뷰들을 반환하는 매서드입니다.
+     *
+     * @param memberId 멤버 ID
+     * @param pageable 조회할 페이지의 정보
+     * @return
+     */
+    @Query("SELECT i FROM Interview i WHERE i.member.id = :memberId")
+    Page<Interview> findByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 
 }
