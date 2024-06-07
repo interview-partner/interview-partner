2841d525e32617b5cf49da8b015edd74f02da4e9
zs0057 <soss4823@gmail.com>
2024-06-07 23:55:54 +0900

    feat: 질문 ID로 답변 찾는 메서드 추가
    #114

--- server/src/main/java/com/vip/interviewpartner/repository/UserAnswerRepository.java
+++ server/src/main/java/com/vip/interviewpartner/repository/UserAnswerRepository.java
@@ -2,9 +2,19 @@ package com.vip.interviewpartner.repository;
 
 import com.vip.interviewpartner.domain.UserAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * UserAnswer 엔티티에 대한 데이터 접근 리포지토리입니다.
  */
 public interface UserAnswerRepository extends JpaRepository<UserAnswer, Long> {
+
+    /**
+     * 질문 ID로 답변 내용을 찾는 메서드
+     *
+     * @param questionId 질문 ID
+     * @return 답변 내용
+     */
+    @Query("SELECT ua.content FROM UserAnswer ua WHERE ua.question.id = :questionId")
+    String findContentByQuestionId(Long questionId);
 }
