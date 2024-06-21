41157eafbe23b2b26513a021ce0f6208bd038829
Daeuning <happyjde3@gmail.com>
2024-06-21 10:27:57 +0900

    feat: 인터뷰 면접 질문 개수 디폴트값 변경
    #150

--- client/src/components/button/counterButton.jsx
+++ client/src/components/button/counterButton.jsx
@@ -31,19 +31,20 @@ export const CounterInput = styled.input`
   border-radius: 5px;
   height: 30px;
   margin: 0 10px;
+  pointer-events: none; // 사용자가 직접 입력하지 못하도록 설정
 `;
 
 const CounterButton = ({ value, onChange }) => {
-  const [counter, setCounter] = useState(value);
+  const [counter, setCounter] = useState(value || 2); // 초기값을 2로 설정
 
   useEffect(() => {
-    setCounter(value);
+    setCounter(value || 2); // 부모 컴포넌트로부터 받은 값이 있을 경우 해당 값으로 설정, 없으면 2로 설정
   }, [value]);
 
   const handleCounterChange = (delta) => {
     setCounter(prevCounter => {
       const newCounter = prevCounter + delta;
-      if (newCounter < 0) return 0;
+      if (newCounter < 2) return 2; // 최소값을 2로 제한
       onChange(newCounter);
       return newCounter;
     });
@@ -51,7 +52,7 @@ const CounterButton = ({ value, onChange }) => {
 
   return (
     <CounterContainer>
-      <StyledCounterButton type="button" onClick={() => handleCounterChange(-1)}>-</StyledCounterButton>
+      <StyledCounterButton type="button" onClick={() => handleCounterChange(-1)} disabled={counter <= 2}>-</StyledCounterButton>
       <CounterInput type="text" value={counter} readOnly />
       <StyledCounterButton type="button" onClick={() => handleCounterChange(1)}>+</StyledCounterButton>
     </CounterContainer>
