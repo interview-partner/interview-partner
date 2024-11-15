602e547fc3fe92b064ea07f14898275e57ad8afc
junest66 <chlwndks333@gmail.com>
2024-11-16 03:15:48 +0900

    docs: 피드백 API 문서화 분리

--- server/src/main/java/com/vip/interviewpartner/domain/feedback/controller/FeedbackController.java
+++ server/src/main/java/com/vip/interviewpartner/domain/feedback/controller/FeedbackController.java
@@ -4,10 +4,6 @@ import com.vip.interviewpartner.common.aop.Login;
 import com.vip.interviewpartner.common.dto.ApiCommonResponse;
 import com.vip.interviewpartner.domain.feedback.dto.request.FeedbackCreateRequest;
 import com.vip.interviewpartner.domain.feedback.service.FeedbackCreateService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,9 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/feedbacks")
-@Tag(name = "feedbacks", description = "피드백 API")
 @Slf4j
-public class FeedbackController {
+public class FeedbackController implements FeedbackApi {
 
     private final FeedbackCreateService feedbackCreateService;
 
@@ -33,19 +28,12 @@ public class FeedbackController {
      * @param request 피드백 생성 요청 정보를 담은 객체
      * @return ApiCommonResponse.successWithNoContent()
      */
-    @Operation(summary = "피드백 생성 API",
-            description = "사용자가 모의면접 간 다른 사용자에게 피드백을 생성합니다.",
-            responses = {
-                    @ApiResponse(responseCode = "201", description = "피드백 생성 성공"),
-                    @ApiResponse(responseCode = "400", description = "유효한 형식 및 요청이 아님, 해당 참가자를 찾지 못하는 경우", content = @Content),
-                    @ApiResponse(responseCode = "409", description = "동일한 참가자한테 이미 피드백을 한 경우", content = @Content),
-            }
-    )
+    @Override
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ApiCommonResponse<?> createFeedback(@Login Long memberId,
                                                @Valid @RequestBody FeedbackCreateRequest request) {
-        feedbackCreateService.createFeedback(memberId, request);
+        feedbackCreateService.create(memberId, request);
         return ApiCommonResponse.successWithNoContent();
     }
 }
