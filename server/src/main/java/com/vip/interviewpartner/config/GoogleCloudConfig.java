19052eeacbf85cb196f0bf7fe3eea41ae0362ac0
junest66 <chlwndks333@gmail.com>
2024-11-29 00:20:20 +0900

    style: 로드 성공 로깅 추가

--- server/src/main/java/com/vip/interviewpartner/config/GoogleCloudConfig.java
+++ server/src/main/java/com/vip/interviewpartner/config/GoogleCloudConfig.java
@@ -3,6 +3,7 @@ package com.vip.interviewpartner.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechSettings;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
+@Slf4j
 public class GoogleCloudConfig {
 
     @Value("${google.cloud.credentials.location}")
@@ -19,6 +21,8 @@ public class GoogleCloudConfig {
 
     @Bean
     public SpeechClient speechClient() throws IOException {
+        log.info("Loading Google Cloud credentials from: {}", gcpCredentials.getFilename());
+
         InputStream credentialsStream = gcpCredentials.getInputStream();
         GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
         SpeechSettings settings = SpeechSettings.newBuilder()
